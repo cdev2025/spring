1ba2632a603329fa7a6b46a327d345f361e5bdf4
cdev2025 <creativedev2025@gmail.com>
2025-10-13 10:52:33 +0900

    [2025-10-13] h2-practice : application.yml 설정

---
+++ h2-practice/src/main/java/com/example/h2_practice/H2PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2PracticeApplication.class, args);
+	}
+
+}
